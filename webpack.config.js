7ac32fc53639e9c64ffb1d88f68527105870bb32
Dmitrii <dimonanos@gmail.com>
2020-08-31 16:35:25 +0300

    finish webpack

---
+++ webpack.config.js
@@ -0,0 +1,127 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const loader = require('sass-loader');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [new OptimizeCssAssetsPlugin(), new TerserPlugin()];
+  }
+
+  return config;
+};
+
+const cssLoaders = ext => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadall: true,
+      },
+    },
+    'css-loader',
+  ];
+
+  if (ext) {
+    loaders.push(ext);
+  }
+
+  return loaders;
+};
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    },
+  ];
+
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+const filename = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'), //папка с исходниками
+  mode: 'development', //по умолчанию
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      // '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 9000,
+    open: 'chrome',
+    hot: isDev,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+};
